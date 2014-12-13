f56b85d05324ae618d1c727508ddfbcf865d4e96
Eyal Ben Ivri <eyalbenivri@gmail.com>
2014-12-13 21:23:23 +0200

    Added loading... functionality to ensure controls are disabled.

--- public/app/couchbase/cac-couchbase-ctrl.js
+++ public/app/couchbase/cac-couchbase-ctrl.js
@@ -1,18 +1,28 @@
 (function() {
     'use strict';
 
-    angular.module('cacApp').controller('cacCouchbaseCtrl', ['$scope', '$location', 'cacIdentity', cacCouchbaseCtrl]);
+    angular.module('cacApp').controller('cacCouchbaseCtrl', ['$scope', '$location', '$timeout', 'cacIdentity', cacCouchbaseCtrl]);
 
-    function cacCouchbaseCtrl($scope, $location, cacIdentity) {
+    function cacCouchbaseCtrl($scope, $location, $timeout, cacIdentity) {
         if(!cacIdentity.isAuthenticated()) {
-            $location.path('/');
+            //$location.path('/');
         }
 
         $scope.cacActionType = "add";
         $scope.cacDryRun = true;
+        $scope.dataSubmit = false;
 
         $scope.showPropertyCustom = function() {
             return $scope.cacActionType == 'change-custom';
+        };
+
+        $scope.submit = function() {
+            $scope.dataSubmit = true;
+            $timeout(function() {
+                var options = { keyboard : false };
+                $('#resultsWindow').modal(options);
+                $scope.dataSubmit = false;
+            }, 3000);
         };
     }
 
